50e2eed2980ab575f602fb32f7a1218d29cf2e25
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-11-26 18:16:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,7 +2,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
 
 def directors_totals(source)
-  puts source[:movies][:title]
   result = {}
   director_index = 0
   while director_index < source.size do
